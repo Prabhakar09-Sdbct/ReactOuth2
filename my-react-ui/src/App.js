e5e3b046e8e56886596b9f0026acbc4c86af4409
Prabhakar Mandloi <pravinsharma699844@gmail.com>
2024-06-22 13:30:22 +0530

    Added material ui template and bootstrap

--- my-react-ui/src/App.js
+++ my-react-ui/src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './Auth/SignIn';
+import SignUp from './Auth/SignUp';
+import LandingPage from './LandingPage';
+import AppAppBar from './components/AppAppBar';
+import getLPTheme from './components/getLPTheme';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
+  const [mode, setMode] = React.useState('light');
+  const LPtheme = createTheme(getLPTheme(mode));
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
   return (
-    <div className="App">
-     Hellow World!
-    </div>
+    <ThemeProvider theme={LPtheme}>
+      <Router>
+        <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+        <Routes>
+          <Route path="/" element={<LandingPage mode={mode}/>} />
+          <Route path="/signIn" element={<SignIn mode={mode}/>} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ my-react-ui/src/LandingPage.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Hero from './components/Hero';
+import LogoCollection from './components/LogoCollection';
+import Highlights from './components/Highlights';
+import Pricing from './components/Pricing';
+import Features from './components/Features';
+import Testimonials from './components/Testimonials';
+import FAQ from './components/FAQ';
+import Footer from './components/Footer';
+import getLPTheme from './components/getLPTheme';
+
+export default function LandingPage({ mode }) {
+  const LPtheme = createTheme(getLPTheme(mode));
+
+  return (
+    <ThemeProvider theme={ LPtheme }>
+      <CssBaseline />
+      <Hero />
+      <Box sx={{ bgcolor: 'background.default' }}>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </Box>
+    </ThemeProvider>
+  );
+}
